74e6c6072ecc83aef0b9f87e39ed56cc4efcf8af
FaridBF <bouras.farid@outlook.fr>
2024-02-09 14:37:28 +0100

    feat: add fontawesome in index

---
+++ src/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+import EmployeesList from './features/EmployeesList/EmployeesList';
+import Error404 from './features/Error404/Error404';
+import Home from './features/Home/Home';
+import './index.css';
+
+library.add(faXmark);
+
+const routes = createBrowserRouter([
+  { errorElement: <Error404 /> },
+  {
+    element: <Home />,
+    path: '/'
+  },
+  {
+    element: <EmployeesList />,
+    path: '/employees'
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={routes} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
