d4d7e61ec7490773a26f31eec2978837572fece1
FaridBF <bouras.farid@outlook.fr>
2024-03-18 11:28:25 +0100

    feat: add a format date function to utils

---
+++ src/utils/Format.jsx
@@ -0,0 +1,19 @@
+/**
+ * Formate un objet Date en chaîne de caractères au format 'YYYY-MM-DD'
+ * @param {Date} date - Objet Date à formater
+ * @returns {string} - Chaîne de caractères formatée
+ */
+function format(date) {
+  console.log('date', date);
+  if (!date || !(date instanceof Date)) return ''; // Si la date est null, undefined ou n'est pas un objet Date, retourne une chaîne vide
+
+  // Obtenir les composants de la date (année, mois, jour)
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Ajoute un zéro initial si le mois est inférieur à 10
+  const day = date.getDate().toString().padStart(2, '0'); // Ajoute un zéro initial si le jour est inférieur à 10
+
+  // Retourne une chaîne de caractères formatée (YYYY-MM-DD par exemple)
+  return `${year}-${month}-${day}`;
+}
+
+export { format };
