9f4c3945e0045c5af3007f08688f102f73f1ff3f
FaridBF <bouras.farid@outlook.fr>
2024-04-08 19:41:19 +0200

    add/librairie/modal

--- src/features/CreateEmployee/CreateEmployee.tsx
+++ src/features/CreateEmployee/CreateEmployee.tsx
@@ -4,9 +4,63 @@ import '../CreateEmployee/CreateEmployee.css';
 
 import { Calendar } from 'primereact/calendar';
 import { Dropdown } from 'primereact/dropdown';
-import { Modal } from '../../components/Modal/Modal';
+// import { Modal } from '../../components/Modal/Modal';
 import { useFormData } from '../../context/CreateEmployeeFormContext';
 import { ListOfStatesAmerican } from '../../service/ListOfStatesAmerican';
+
+// import { Modal } from 'fb-modal-component-library/dist/Modal';
+const Modal = require('fb-modal-component-library/dist/Modal');
+const closeButtonImg = require('../../assets/closeButton.png');
+
+// ==============================
+// LIBRAIRIE MODALE
+// ==============================
+
+// import '../../components/Modal/modal.css';
+
+// interface ModalProps {
+//   isVisible: boolean;
+//   title: string;
+//   description?: string;
+//   src: string;
+//   onClose: () => void;
+// }
+
+// /**
+//  * Affiche une fenêtre modale avec un message.
+//  * @param {object} props - Les propriétés du composant.
+//  * @param {function} props.setModalOpen - Fonction pour définir l'état d'ouverture de la modal.
+//  */
+// const Modal: React.FC<ModalProps> = ({
+//   isVisible,
+//   title,
+//   description,
+//   src,
+//   onClose
+// }) => {
+//   if (!isVisible) {
+//     return null; // Ne rien rendre si isVisible est à false
+//   }
+
+//   return (
+//     <div className='modalBackground'>
+//       <div className='modalContainer'>
+//         <div className='rightPart'>
+//           <div className='titleRightPart'>
+//             <h1 className='title-employee-created'>{title}</h1>
+//             <button className='button' onClick={onClose}>
+//               <img src={src} alt='Close Modal' />
+//             </button>
+//           </div>
+//           <div className='descriptionModal'>
+//             <p>{description}</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// ==============================
 
 interface StateOption {
   label: string;
@@ -74,6 +128,11 @@ const CreateEmployee: React.FC = () => {
       return;
     }
     setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+    // document.body.classList.remove('modal-open');
   };
 
   return (
@@ -176,7 +235,13 @@ const CreateEmployee: React.FC = () => {
         </button>
       </form>
 
-      {modalOpen && <Modal setModalOpen={setModalOpen} />}
+      <Modal
+        title='Employee created'
+        description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus elit libero, at finibus dolor auctor id. Aliquam ut lectus vitae odio tincidunt blandit. Vivamus cursus, lorem ut congue rutrum, lectus eros tristique lectus, vitae imperdiet massa purus a orci. Donec nibh'
+        src={closeButtonImg}
+        isVisible={modalOpen}
+        onClose={handleCloseModal}
+      />
     </>
   );
 };
