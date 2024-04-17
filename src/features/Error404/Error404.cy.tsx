4c00b00c06c38baa134cc91683b81d38f5024b6d
FaridBF <bouras.farid@outlook.fr>
2024-04-17 14:54:14 +0200

    test: adding data-cy in the error 404 page with a test battery

---
+++ src/features/Error404/Error404.cy.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { mount } from '@cypress/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Error404 from './Error404';
+
+import '../../index.scss';
+
+describe('<Error404 />', () => {
+  it('renders', () => {
+    mount(
+      <Router>
+        <Error404 />
+      </Router>
+    );
+
+    cy.get('[data-cy="error404-container"]').should('exist');
+    cy.get('[data-cy="error404-title"]').should('contain', 'Error 404');
+    cy.get('[data-cy="error404-description"]').should(
+      'contain',
+      'Sorry! This page does not exist.'
+    );
+    cy.get('[data-cy="error404-link"]').should('have.attr', 'href', '/');
+  });
+
+  it('navigates back to home page when link is clicked', () => {
+    mount(
+      <Router>
+        <Error404 />
+      </Router>
+    );
+
+    cy.get('[data-cy="error404-link"]').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/');
+  });
+});
--- src/features/Error404/Error404.tsx
+++ src/features/Error404/Error404.tsx
@@ -15,13 +15,15 @@ interface Error404Props {}
 const Error404: FunctionComponent<Error404Props> = (props) => {
   return (
     <>
-      <div className='error404__container'>
-        <h1 className='error404__title'>Error 404</h1>
-        <h2 className='error404__description'>
+      <div className='error404__container' data-cy='error404-container'>
+        <h1 className='error404__title' data-cy='error404-title'>
+          Error 404
+        </h1>
+        <h2 className='error404__description' data-cy='error404-description'>
           Sorry! This page does not exist.
         </h2>
 
-        <Link className='error404__link' to={'/'}>
+        <Link className='error404__link' to={'/'} data-cy='error404-link'>
           Back to home
         </Link>
       </div>
