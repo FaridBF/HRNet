0e880af8b959896304a6b3a26724eeebd7bf6228
FaridBF <bouras.farid@outlook.fr>
2024-05-30 17:00:26 +0200

    test: cypress test finalization

--- src/components/Modal/Modal.cy.tsx
+++ src/components/Modal/Modal.cy.tsx
@@ -1,111 +1,111 @@
-// import React, { useState } from 'react';
-// import { mount } from 'cypress/react18';
-// import Modal from './Modal';
+import React, { useState } from 'react';
+import { mount } from 'cypress/react18';
+import Modal from './Modal';
 
-// import '../../index.scss';
+import '../../index.scss';
 
-// const defaultImageCloseModal = require('../../assets/icons/defaultImageCloseModal.png');
-// const closeButtonImg = require('../../assets/icons/closeButton.png');
+const defaultImageCloseModal = require('../../assets/icons/defaultImageCloseModal.png');
+const closeButtonImg = require('../../assets/icons/closeButton.png');
 
-// // Composant temporaire intermédiaire pour tester
-// function TestComponent({
-//   title = 'Employee created',
-//   description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-// }) {
-//   const [modalOpen, setModalOpen] = useState(true);
+// Composant temporaire intermédiaire pour tester
+function TestComponent({
+  title = 'Employee created',
+  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+}) {
+  const [modalOpen, setModalOpen] = useState(true);
 
-//   const handleCloseModal = () => {
-//     setModalOpen(false);
-//   };
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
 
-//   return (
-//     <Modal
-//       title={title}
-//       description={description}
-//       isVisible={modalOpen}
-//       onClose={handleCloseModal}
-//       src={closeButtonImg}
-//     />
-//   );
-// }
+  return (
+    <Modal
+      title={title}
+      description={description}
+      isVisible={modalOpen}
+      onClose={handleCloseModal}
+      src={closeButtonImg}
+    />
+  );
+}
 
-// interface DefaultProps {
-//   title: string;
-//   description: string;
-// }
+interface DefaultProps {
+  title: string;
+  description: string;
+}
 
-// describe('<Modal />', () => {
-//   let defaultProps: DefaultProps;
+describe('<Modal />', () => {
+  let defaultProps: DefaultProps;
 
-//   beforeEach(() => {
-//     defaultProps = {
-//       title: 'Employee created',
-//       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-//     };
-//   });
+  beforeEach(() => {
+    defaultProps = {
+      title: 'Employee created',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    };
+  });
 
-//   it('renders with default props', () => {
-//     mount(<TestComponent {...defaultProps} />);
-//     cy.get('[data-cy="modal"]').should('exist');
-//     cy.get('[data-cy="modal-title"]').should('contain', defaultProps.title);
-//     cy.get('[data-cy="modal-description"]').should(
-//       'contain',
-//       defaultProps.description
-//     );
-//   });
+  it('renders with default props', () => {
+    mount(<TestComponent {...defaultProps} />);
+    cy.get('[data-cy="modal"]').should('exist');
+    cy.get('[data-cy="modal-title"]').should('contain', defaultProps.title);
+    cy.get('[data-cy="modal-description"]').should(
+      'contain',
+      defaultProps.description
+    );
+  });
 
-//   it('is initially visible', () => {
-//     mount(<TestComponent {...defaultProps} />);
-//     cy.get('[data-cy="modal"]').should('be.visible');
-//   });
+  it('is initially visible', () => {
+    mount(<TestComponent {...defaultProps} />);
+    cy.get('[data-cy="modal"]').should('be.visible');
+  });
 
-//   it('closes when the close button is clicked', () => {
-//     mount(<TestComponent {...defaultProps} />);
-//     cy.get('[data-cy="close-button"]').click();
-//     cy.get('[data-cy="modal"]').should('not.exist');
-//   });
+  it('closes when the close button is clicked', () => {
+    mount(<TestComponent {...defaultProps} />);
+    cy.get('[data-cy="close-button"]').click();
+    cy.get('[data-cy="modal"]').should('not.exist');
+  });
 
-//   it('updates when props change', () => {
-//     const updatedProps = {
-//       title: 'Employee updated',
-//       description: 'Updated description.'
-//     };
+  it('updates when props change', () => {
+    const updatedProps = {
+      title: 'Employee updated',
+      description: 'Updated description.'
+    };
 
-//     mount(<TestComponent {...defaultProps} />);
-//     cy.get('[data-cy="modal-title"]').should('contain', defaultProps.title);
-//     cy.get('[data-cy="modal-description"]').should(
-//       'contain',
-//       defaultProps.description
-//     );
+    mount(<TestComponent {...defaultProps} />);
+    cy.get('[data-cy="modal-title"]').should('contain', defaultProps.title);
+    cy.get('[data-cy="modal-description"]').should(
+      'contain',
+      defaultProps.description
+    );
 
-//     mount(<TestComponent {...updatedProps} />);
-//     cy.get('[data-cy="modal-title"]').should('contain', updatedProps.title);
-//     cy.get('[data-cy="modal-description"]').should(
-//       'contain',
-//       updatedProps.description
-//     );
-//   });
+    mount(<TestComponent {...updatedProps} />);
+    cy.get('[data-cy="modal-title"]').should('contain', updatedProps.title);
+    cy.get('[data-cy="modal-description"]').should(
+      'contain',
+      updatedProps.description
+    );
+  });
 
-//   it('renders with missing props', () => {
-//     const missingProps: DefaultProps = {
-//       title: 'Default Title',
-//       description: 'Default description.'
-//     };
+  it('renders with missing props', () => {
+    const missingProps: DefaultProps = {
+      title: 'Default Title',
+      description: 'Default description.'
+    };
 
-//     mount(
-//       <Modal
-//         isVisible={true}
-//         onClose={() => {}}
-//         title={missingProps.title}
-//         description={missingProps.description}
-//         src={defaultImageCloseModal}
-//       />
-//     );
-//     cy.get('[data-cy="modal"]').should('exist');
-//     cy.get('[data-cy="modal-title"]').should('contain', missingProps.title);
-//     cy.get('[data-cy="modal-description"]').should(
-//       'contain',
-//       missingProps.description
-//     );
-//   });
-// });
+    mount(
+      <Modal
+        isVisible={true}
+        onClose={() => {}}
+        title={missingProps.title}
+        description={missingProps.description}
+        src={defaultImageCloseModal}
+      />
+    );
+    cy.get('[data-cy="modal"]').should('exist');
+    cy.get('[data-cy="modal-title"]').should('contain', missingProps.title);
+    cy.get('[data-cy="modal-description"]').should(
+      'contain',
+      missingProps.description
+    );
+  });
+});
--- src/features/Error404/Error404.cy.tsx
+++ src/features/Error404/Error404.cy.tsx
@@ -1,36 +1,36 @@
-// import React from 'react';
-// import { mount } from 'cypress/react18';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
+import { mount } from 'cypress/react18';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-// import Error404 from './Error404';
+import Error404 from './Error404';
 
-// import '../../index.scss';
+import '../../index.scss';
 
-// describe('<Error404 />', () => {
-//   it('renders', () => {
-//     mount(
-//       <Router>
-//         <Error404 />
-//       </Router>
-//     );
+describe('<Error404 />', () => {
+  it('renders', () => {
+    mount(
+      <Router>
+        <Error404 />
+      </Router>
+    );
 
-//     cy.get('[data-cy="error404-container"]').should('exist');
-//     cy.get('[data-cy="error404-title"]').should('contain', 'Error 404');
-//     cy.get('[data-cy="error404-description"]').should(
-//       'contain',
-//       'Sorry! This page does not exist.'
-//     );
-//     cy.get('[data-cy="error404-link"]').should('have.attr', 'href', '/');
-//   });
+    cy.get('[data-cy="error404-container"]').should('exist');
+    cy.get('[data-cy="error404-title"]').should('contain', 'Error 404');
+    cy.get('[data-cy="error404-description"]').should(
+      'contain',
+      'Sorry! This page does not exist.'
+    );
+    cy.get('[data-cy="error404-link"]').should('have.attr', 'href', '/');
+  });
 
-//   it('navigates back to home page when link is clicked', () => {
-//     mount(
-//       <Router>
-//         <Error404 />
-//       </Router>
-//     );
+  it('navigates back to home page when link is clicked', () => {
+    mount(
+      <Router>
+        <Error404 />
+      </Router>
+    );
 
-//     cy.get('[data-cy="error404-link"]').click();
-//     cy.url().should('eq', Cypress.config().baseUrl + '/');
-//   });
-// });
+    cy.get('[data-cy="error404-link"]').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/');
+  });
+});
